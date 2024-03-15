e95d48581c990811abb154319e2cf16efd4fc164
daothanhbinh20 <thanhbinh1992000@gmail.com>
2024-03-15 00:23:07 +0000

    feat: add CustomMetrics template

---
+++ src/main/java/com/bookingsystem/accountservice/metrics/CustomMetrics.java
@@ -0,0 +1,14 @@
+package com.bookingsystem.accountservice.metrics;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CustomMetrics {
+    private final MeterRegistry meterRegistry;
+
+    public CustomMetrics(final MeterRegistry meterRegistry){
+        this.meterRegistry = meterRegistry;
+    }
+}
